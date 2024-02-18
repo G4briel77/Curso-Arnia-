9782efed7f3d89a07cf73c4bbed33286fdf629d2
Gabriel B4tista <114261033+G4briel77@users.noreply.github.com>
2024-02-18 18:27:31 -0300

    Delete Arnia/curso Arnia directory

--- Arnia/curso Arnia/meu site/ex01.js
+++
@@ -1,9 +0,0 @@
-const cliqueBotao = () => {
-    alert('O botão foi clicado!')
-}
-
-//recuperando o elemento da página HTMl através do DOM
-const btn = document.getElementById('btn')
-btn.addEventListener('click', () => {
-    btn.classList.add('vermelho')
-})
