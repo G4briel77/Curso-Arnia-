dfee6dfdc696d5a69292dbfa314a86e2abe95a1f
G4briel77 <gabriel.batista1999@hotmail.com>
2024-02-18 18:38:02 -0300

    update entrega

--- Arnia/curso Arnia/fnanonina02.js
+++
@@ -1,8 +0,0 @@
-const io = require('../curso Arnia/io/io')
-
-const notas = [10,10,10,5.5,6.4,3,4,9,8,6.5,3]
-
-const somarNotas = notas.reduce((valorAnterior,valorAtual)=>{
-    return valorAnterior + valorAtual
-}, 0)
-console.log(somarNotas)
